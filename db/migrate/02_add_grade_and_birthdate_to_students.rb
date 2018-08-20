dfca185b58817efd9338bfff4b257789fa0e23ee
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-20 14:07:23 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,8 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   
   def change
-    add_column :artists, :grade, :integer, :birthdate, :string 
+    add_column :artists, :grade, :integer 
+    add_column :artists, :birthdate, :string 
   end 
   
 end 
